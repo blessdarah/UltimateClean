cce9a045331b31b36e3fdf359a75fc450944ec68
= <blessdarahuba@gmail.com>
2023-10-21 09:12:10 +0100

    chore: ignore

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,50 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <div class="flex justify-between px-8 space-x-4 w-full">
-                <a href="/" class="py-2">
-                    <x-application-logo class="w-10 h-10 fill-current text-gray-500" />
-                </a>
-                <div class="inline-flex items-center space-x-4">
-                    <x-nav-link href="/welcome">{{__('Home')}}</x-nav-link>
-                    <x-nav-link>{{__('Services')}}</x-nav-link>
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button class="inline-flex items-center border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{__('About us')}}</div>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-                                <div class="ml-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-                        <x-slot name="content">
-                            <x-dropdown-link href="welcome">
-                                {{ __('Services') }}
-                            </x-dropdown-link>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
+<body class="font-sans text-gray-900 antialiased">
+    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        {{-- navigation --}}
+        <div class="flex justify-between px-8 space-x-4 w-full max-w-7xl mx-auto">
+            <a href="/" class="py-2 inline-flex items-center space-x-2">
+                <x-application-logo class="w-10 h-10 fill-current text-gray-500" />
+                <span class="font-semibold text-gray-600">LOGO</span>
+            </a>
+            <div class="inline-flex items-center space-x-4">
+                <x-nav-link href="/welcome">{{ __('Home') }}</x-nav-link>
+                <x-nav-link>{{ __('Services') }}</x-nav-link>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button
+                            class="inline-flex items-center border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            <div>{{ __('About us') }}</div>
+
+                            <div class="ml-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link href="{{ route('welcome') }}">
+                            {{ __('Services') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
             </div>
-
-            {{ $slot }}
         </div>
-    </body>
+
+        {{ $slot }}
+    </div>
+</body>
+
 </html>
